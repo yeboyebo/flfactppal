7eedce0c46bc40ff0bae986e52efebbb31c83641
Nikolay-Yeboyebo <nikolay@yeboyebo.es>
2022-10-06 10:24:31 +0200

    Añadir funcion llamada a PinebooApi

--- model_flfactppal__flfactppal_def.py
+++ model_flfactppal__flfactppal_def.py
@@ -19,6 +19,20 @@ class flfactppal(interna):
     def __init__(self, context=None):
         super().__init__(context)
 
+    def llamaAPI(self, url, verbo, params=None):
+        res = {}
+        api_url = "http://127.0.0.1:8005/api/"
+        # api_url = "http://172.65.0.1:8005/api/"
+        token = 'd12e36e5b04b5c060451722c460c47a1178fd61e'
+        header = {"Content-Type": "application/json", "Authorization": "Token {}".format(token)}
+        if verbo == "post":
+            res = requests.post(api_url + url, headers=header, data=params)
+        elif verbo == "get":
+            # Esta sin probar de momento. Fecha 06-10-2022
+            res = requests.get(api_url + url, headers=header, params=params)
+
+        return res
+
 
 # @class_declaration head #
 class head(flfactppal):
